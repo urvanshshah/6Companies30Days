41029792831afd28641c84f1a377d76bf256247d
Urvansh Shah <67900726+urvanshshah@users.noreply.github.com>
2022-01-01 17:58:42 +0530

    Update 1:Given an array of strings, return all groups of strings that are anagrams.py

--- Goldman Sachs/1:Given an array of strings, return all groups of strings that are anagrams.py
+++ Goldman Sachs/1:Given an array of strings, return all groups of strings that are anagrams.py
@@ -1,1 +1,20 @@
-# d1 p1 : Given an array of strings, return all groups of strings that are anagrams.
+# 1:Given an array of strings, return all groups of strings that are anagrams
+
+class Solution:
+    def Anagrams(self, words, n):
+        def ss(s):
+            count = [0]*26
+            for i, c in enumerate(s):
+                count[ord(c)-ord('a')] += 1
+            ans = ""
+            for i in range(26):
+                ans+= chr(ord('a')+i)*count[i]
+            return ans
+        anagram = {}      
+        for i,string in enumerate(words):
+            ascString = ss(string)
+            if ascString not in anagram:
+                anagram[ascString] = []
+            anagram[ascString].append(words[i])
+        return [val for val in anagram.values()]
+        n = 256
