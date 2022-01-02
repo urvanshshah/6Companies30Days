dafdc8cfc2e433d2050f1ef5210b8369d6347d36
Urvansh Shah <67900726+urvanshshah@users.noreply.github.com>
2022-01-03 00:12:18 +0530

    Update 2:Overlapping rectangles.py

--- Goldman Sachs/2:Overlapping rectangles.py
+++ Goldman Sachs/2:Overlapping rectangles.py
@@ -1,1 +1,12 @@
+##2 : Overlapping rectangles
 
+class Solution:
+
+    def doOverlap(self, L1, R1, L2, R2):
+        
+        if  R2[1] >L1[1] or R1[1]>L2[1]:
+            return 0
+        if   L2[0]> R1[0]  or L1[0] > R2[0]:
+            return 0
+           
+        return 1 
