b5950230ff82e9a5e4a6ab66645132c51604e8c0
Urvansh Shah <67900726+urvanshshah@users.noreply.github.com>
2022-01-03 11:06:00 +0530

    Update GS03.py

--- Goldman Sachs/GS03.py
+++ Goldman Sachs/GS03.py
@@ -1,1 +1,17 @@
+##3 : Count the subarrays having product less than k
 
+class Solution:
+    def countSubArrayProductLessThanK(self, a, n, k):
+        #Code here
+        c = 0
+        for i in range(0, n):
+            if a[i] < k:
+                c += 1
+            m = a[i]
+            for j in range(i + 1, n):
+                m = m * a[j]
+                if m < k:
+                    c += 1
+                else:
+                    break
+        return c
