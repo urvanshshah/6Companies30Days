047c3a905d2be98228f05f57efba9d2d1380b0cc
Urvansh Shah <67900726+urvanshshah@users.noreply.github.com>
2022-01-03 11:31:50 +0530

    Update GS07.py

--- Goldman Sachs/GS07.py
+++ Goldman Sachs/GS07.py
@@ -1,1 +1,9 @@
+## 7 : Distributing M items in a circle of size N starting from K-th position
 
+def lastPosition(n, m, k):
+    return (k + (m % n) -1)
+
+n = int(input())  #5
+m = int(input())  #8
+k = int(input())  #2
+print(lastPosition(n, m, k))  #4
