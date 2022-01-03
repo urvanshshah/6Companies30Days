d7909e0003a5c418239dd8bc30b3a8e52b1dc117
Urvansh Shah <67900726+urvanshshah@users.noreply.github.com>
2022-01-03 10:47:55 +0530

    Update GS05.py

--- Goldman Sachs/GS05.py
+++ Goldman Sachs/GS05.py
@@ -1,1 +1,20 @@
+##5 : Program to find Nth Ugly Number
 
+class Solution:
+	def getNthUglyNo(self,n):
+		# code here
+    	ugly = [1]
+        i2 = i3 = i5 = 0
+    
+        for i in range(1,n):
+            mini = min(ugly[i2]*2,ugly[i3]*3,ugly[i5]*5)
+    
+            ugly.append(mini)
+            if mini == ugly[i2]*2:
+                i2+=1
+            if mini == ugly[i3]*3:
+                i3+=1
+            if mini == ugly[i5]*5:
+                i5+=1
+        
+        return ugly[n-1]
