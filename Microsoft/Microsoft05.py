91eeddad745fdfe2db2ea4051d99d713938ceea4
Urvansh Shah <67900726+urvanshshah@users.noreply.github.com>
2022-01-14 16:06:04 +0530

    Update Microsoft05.py

--- Microsoft/Microsoft05.py
+++ Microsoft/Microsoft05.py
@@ -1,13 +1,22 @@
 ##5 : Stock span problem 
 
 class Solution:
-    def calculateSpan(self,a,n):
-        s = []
+    def calculateSpan(self, a, n):
+        st = []
+        def helper(price):
+            if not st:
+                st.append((price,1))
+                return 1
+            else:
+                ans = 1
+                while st and st[-1][0]<=price:
+                    t = st.pop()
+                    ans+=t[1]
+                st.append((price,ans))
+            return ans
+        
+        res = []
         for i in range(n):
-            c = 1
-            j = i-1
-            while j>=0 and a[j] <=a[i]:
-                c +=1
-                j-=1
-            s.append(c)    
-        return s 
+            res.append(helper(a[i]))
+        return res     
+
