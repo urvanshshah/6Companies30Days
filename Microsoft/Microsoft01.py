6624701165450a634bceef648c7d278bdb7c218c
Urvansh Shah <67900726+urvanshshah@users.noreply.github.com>
2022-01-16 15:33:25 +0530

    Update Microsoft01.py

--- Microsoft/Microsoft01.py
+++ Microsoft/Microsoft01.py
@@ -1,1 +1,24 @@
+## 1:Minimum sum partition
 
+class Solution{
+
+  public:
+	int minDifference(int arr[], int n)  { 
+	    // Your code goes here
+	    int sum = 0;
+	    for(int i=0; i<n; i++) sum+=arr[i];
+	    
+	    int dp[n+1][(sum/2)+1];
+	    
+	    for(int i=0; i<=n; i++){
+	        for(int j=0; j<=sum/2; j++){
+	            if(j==0) dp[i][j] = 0;
+	            else if(i==0) dp[i][j] = j;
+	            else if(arr[i-1]>j) dp[i][j] = dp[i-1][j];
+	            else dp[i][j] = min(dp[i-1][j], dp[i-1][j-arr[i-1]]);
+	        }
+	    }
+	    
+	    return (sum%2) + 2*dp[n][sum/2];
+	} 
+};
