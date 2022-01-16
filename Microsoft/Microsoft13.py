7a2b59368f20448e387d83910ea98feb65747839
Urvansh Shah <67900726+urvanshshah@users.noreply.github.com>
2022-01-17 01:22:20 +0530

    Update Microsoft13.py

--- Microsoft/Microsoft13.py
+++ Microsoft/Microsoft13.py
@@ -1,1 +1,25 @@
+## 13 : Bridge edge in a graph
 
+from queue import Queue
+class Solution:
+    def isBridge(self, V, adj, c, d):
+        try:
+            adj[c].remove(d)
+        except:
+            return 0
+        try:
+            adj[d].remove(c)
+        except:
+            return 0
+        visited = set()
+        queue = Queue()
+        queue.put(c)
+        while not queue.empty():
+            out = queue.get()
+            visited.add(out)
+            for neighbour in adj[out]:
+                if neighbour not in visited:
+                    queue.put(neighbour)
+        if d not in visited:
+            return 1
+        return 0
