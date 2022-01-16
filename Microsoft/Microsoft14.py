ad19fa2d5c26e5395093bcc743e32ad018777b74
Urvansh Shah <67900726+urvanshshah@users.noreply.github.com>
2022-01-17 01:27:52 +0530

    Update Microsoft14.py

--- Microsoft/Microsoft14.py
+++ Microsoft/Microsoft14.py
@@ -1,1 +1,24 @@
+## 14 : Minimum steps to destination
 
+class Solution:
+    def minSteps(self, D):
+        i = 1
+        def select(sum,i,count):
+            if sum>D or sum<-D:
+                return 1e7
+            if sum==D:
+                return count
+            count += 1
+            return min(select(sum-i,i+1,count),select(sum+i,i+1,count))
+        return select(0,i,0)
+    
+    def minSteps2(D):
+        target = abs(D)
+        step = 0
+        sum = 0
+        
+        while sum<target or (sum-target)%2!=0:
+            step+=1
+            sum += step
+        
+        return step
