7755ed98f1dbbfae73a3f84143b765443f929724
Urvansh Shah <67900726+urvanshshah@users.noreply.github.com>
2022-01-21 00:20:12 +0530

    Create Adobe09.py

---
+++ Adobe/Adobe09.py
@@ -0,0 +1,23 @@
+// 9 : Next higher palindromic number using the same set of digits
+
+class Solution{
+  public:
+    string nextPalin(string s) { 
+        //complete the function here
+        int n=s.size();
+        if(n<=3) return "-1";
+        string half = s.substr(0,n/2);
+        if( next_permutation( half.begin() , half.end() ) )
+        {
+            string ans=half;
+            if(n&1)
+            {
+                ans+=s[n/2];
+            }
+            reverse(half.begin() , half.end());
+            ans+=half;
+            return ans;
+        }
+        return "-1";
+    }
+};
