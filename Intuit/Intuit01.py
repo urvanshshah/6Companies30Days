191bddd477a2be9a20e3f0318eda18cf7f09e081
Urvansh Shah <67900726+urvanshshah@users.noreply.github.com>
2022-01-25 13:32:25 +0530

    Update Intuit01.py

--- Intuit/Intuit01.py
+++ Intuit/Intuit01.py
@@ -1,1 +1,17 @@
+## 1: Partition a set into two subsets such that the difference of subset sums is minimum
 
+def findMinRec(arr, i, sumCalculated, sumTotal):
+
+    if (i == 0):
+        return abs((sumTotal - sumCalculated) - sumCalculated)
+
+    return min(findMinRec(arr, i - 1, sumCalculated+arr[i - 1], sumTotal), findMinRec(arr, i - 1, sumCalculated, sumTotal))
+
+ 
+def findMin(arr,  n):
+
+    sumTotal = 0
+    for i in range(n):
+        sumTotal += arr[i]
+
+    return findMinRec(arr, n, 0, sumTotal)
